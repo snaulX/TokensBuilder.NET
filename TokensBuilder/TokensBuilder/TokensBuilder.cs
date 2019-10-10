137a5577c42ac7dd04bb4671dd1cf8f42f0df073
snaulX <alexandergunger666@gmail.com>
2019-10-10 19:20:50 +0300

    Create TokensBuilder

---
+++ TokensBuilder/TokensBuilder/TokensBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace TokensBuilder
+{
+    public class TokensBuilder
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                //пока ничего не делать
+            }
+            else
+            {
+                switch (args[0])
+                {
+                    case "-o":
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public void Build(string assembly_name, string code)
+        {
+            AssemblyBuilder assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(assembly_name), AssemblyBuilderAccess.Run);
+        }
+    }
+}
