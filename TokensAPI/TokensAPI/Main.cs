c27d1735d9830ef68236511af81009cf3078620f
snaulX <alexandergunger666@gmail.com>
2019-10-10 07:14:08 +0300

    Add TokensAPI project to repository

---
+++ TokensAPI/TokensAPI/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TokensAPI
+{
+    public static class Main
+    {
+        public static List<Token> tokens
+        {
+            get => new List<Token>();
+        }
+    }
+}
---
+++ TokensAPI/TokensAPI/Token.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TokensAPI
+{
+    public abstract class Token
+    {
+        public abstract Identifer Parse(params Identifer[] identifers);
+    }
+}
