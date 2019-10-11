fb2d0138d96bf9e85e2ac4a49a48adfca4c10ed3
snaulX <alexandergunger666@gmail.com>
2019-10-11 06:54:55 +0300

    Add reading code from file and build him

--- TokensBuilder/TokensBuilder/TokensBuilder.cs
+++ TokensBuilder/TokensBuilder/TokensBuilder.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 using System.Reflection.Emit;
 
 namespace TokensBuilder
 {
-    public class TokensBuilder
+    public static class TokensBuilder
     {
         static void Main(string[] args)
         {
@@ -17,16 +18,26 @@ namespace TokensBuilder
                 switch (args[0])
                 {
                     case "-o":
+                        string filename = args[1];
+                        using (StreamReader file = File.OpenText(Path.GetFullPath(filename)))
+                        {
+                            Build(filename.Remove(filename.LastIndexOf('.')), file.ReadToEnd());
+                        }
                         break;
                     default:
+                        filename = args[0];
+                        using (StreamReader file = File.OpenText(Path.GetFullPath(filename)))
+                        {
+                            Build(filename.Remove(filename.LastIndexOf('.')), file.ReadToEnd());
+                        }
                         break;
                 }
             }
         }
 
-        public void Build(string assembly_name, string code)
+        public static void Build(string assembly_name, string code)
         {
-            AssemblyBuilder assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(assembly_name), AssemblyBuilderAccess.Run);
+            AssemblyBuilder assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(assembly_name), AssemblyBuilderAccess.RunAndCollect);
         }
     }
 }
