f51c6ef47833b26bc285f86f2dfd387408f39144
snaulX <alexandergunger666@gmail.com>
2019-10-20 19:00:36 +0300

    Create LongIdentifer

---
+++ TokensAPI/TokensAPI/identifers/LongIdentifer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TokensAPI.identifers
+{
+    public class LongIdentifer : Identifer
+    {
+        public LongIdentifer()
+        {
+            identifer = "";
+        }
+
+        public LongIdentifer(string identifer): this()
+        {
+            if (Check(identifer)) this.identifer = identifer;
+        }
+
+        public override bool Check(string input) => input.StartsWith('"') && input.EndsWith('"');
+
+        public override void Parse(string input)
+        {
+            if (Check(input)) identifer = input;
+        }
+
+        public string GetValue() => identifer.Remove(0, 1).Remove(identifer.Length - 2);
+    }
+}
