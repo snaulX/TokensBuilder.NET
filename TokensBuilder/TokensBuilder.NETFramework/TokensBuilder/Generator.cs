5c5443adb62cc4baf931c3ee5f36becd45ed7bff
snaulX <alexandergunger666@gmail.com>
2019-11-14 08:34:49 +0300

    Create Generator and link Mono.Cecil

---
+++ TokensBuilder/TokensBuilder.NETFramework/TokensBuilder/Generator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+
+namespace TokensBuilder
+{
+    public class Generator
+    {
+        public ContextInfo context;
+        public List<Expression> expressions;
+
+        public Generator()
+        {
+            expressions = new List<Expression>();
+            context = new ContextInfo();
+        }
+
+        public void GenerateIL(string assembly_name)
+        {
+            AssemblyNameDefinition assemblyName = new AssemblyNameDefinition(assembly_name, new Version());
+        }
+    }
+}
