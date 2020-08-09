49ba54aa2c10274e768482692a9142c2e0242a10
snaulX <alexandergunger666@gmail.com>
2020-08-09 12:59:51 +0300

    Rework TokensTemplate and create IncludeTemplate

---
+++ TokensBuilder/Templates/IncludeTemplate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using TokensAPI;
+using TokensBuilder.Errors;
+
+namespace TokensBuilder.Templates
+{
+    class IncludeTemplate : TokensTemplate
+    {
+        public string libPath = "";
+
+        public bool Parse(TokensReader expression, bool expression_end)
+        {
+            return expression_end && expression.tokens == new List<TokenType> { TokenType.INCLUDE };
+        }
+
+        public List<TokensError> Run(TokensReader expression)
+        {
+            List<TokensError> errors = new List<TokensError>();
+            libPath = expression.string_values.Peek();
+            uint line = TokensBuilder.gen.line;
+            try
+            {
+                Assembly.LoadFrom(libPath);
+            }
+            catch (FileNotFoundException)
+            {
+                errors.Add(new IncludeError(line, $"The {libPath} was not found, or the module" +
+                    " you are trying to load does not indicate a file name extension."));
+            }
+            catch (FileLoadException)
+            {
+                errors.Add(new IncludeError(line, "Failed to load the file that was found." +
+                    " or The ability to execute code in remote assemblies is disabled."));
+            }
+            catch (BadImageFormatException)
+            {
+                errors.Add(new IncludeError(line, $"{Path.GetFileName(libPath)} is not valid assembly"));
+            }
+            catch (ArgumentException)
+            {
+                errors.Add(new IncludeError(line, $"Name of assembly is empty or not valid"));
+            }
+            catch (PathTooLongException)
+            {
+                errors.Add(new IncludeError(line, "The assembly name is longer than the maximum length" +
+                    " defined in the system."));
+            }
+            return errors;
+        }
+    }
+}
--- TokensBuilder/TokensTemplate.cs
+++ TokensBuilder/TokensTemplate.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using TokensAPI;
 
 namespace TokensBuilder
 {
     interface TokensTemplate
     {
-        bool Parse(ref Generator gen);
+        /// <summary>
+        /// Parse expression by template
+        /// </summary>
+        /// <param name="expression">Getted expression</param>
+        /// <param name="expression_end">After getting expression has expression end (true) or open block (false)</param>
+        /// <returns>Getted expression equals current template</returns>
+        bool Parse(TokensReader expression, bool expression_end);
+
+        /// <summary>
+        /// Run expression by this template
+        /// </summary>
+        /// <param name="expression">Expression for run</param>
+        /// <returns>List of errors when was throwed in running</returns>
+        List<TokensError> Run(TokensReader expression);
     }
 }
